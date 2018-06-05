73d6ab2a9c99249a45040f2cdf6bd8bfd4d01c31
Lorena <lorena.giddens@outlook.com>
2018-06-05 13:19:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  # else
+  else
+    line_size = Array.new
+    katz_deli.each do |element, index|
+      line_size << katz_deli[index].insert(0, "#{index + 1}.")
   end
+  puts "The line is currently: #{line_size.join(" ")}"
 end
